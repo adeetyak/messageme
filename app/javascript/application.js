78e7f4e6b0dcd599a87f7584850b41988ca8089c
Aditya <mr.adityakaushal@gmail.com>
2022-01-16 18:30:31 +0530

    Favicon added and navigation partial

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+// import "@hotwired/turbo-rails"
+import './src/jquery'
+import '@doabit/semantic-ui-sass'
+import "./controllers"
+
+$(document).ready(function() {
+    // executes when HTML-Document is loaded and DOM is ready
+    $('.ui.dropdown').dropdown();
+});
